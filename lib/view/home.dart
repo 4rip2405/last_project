209049cc4e51fdd8120bb422f335ac3cfba1a941
4rif2405 <arifpaturrahman3@gmail.com>
2023-07-05 12:15:13 +0700

    kerangka

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,23 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:last_project/view/setting.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class home extends StatefulWidget {
+  const home({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<home> createState() => _homeState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        title: Text('Simple Home'),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: 'Calls',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            label: 'Camera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Simple Home!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
           children: [
-            Icon(Icons.restaurant_menu),
-            SizedBox(width: 10),
-            Text("RESEP MAKANAN")
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Simple Home',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                // Navigasi ke halaman utama
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Navigasi ke halaman pengaturan
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SettingsPage()));
+              },
+            ),
           ],
         ),
       ),
