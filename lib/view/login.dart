67c58474e8b48d293440194e71c5b9f6018f01e7
4rif2405 <arifpaturrahman3@gmail.com>
2023-07-12 13:28:59 +0700

    login edit

---
+++ lib/view/login.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:last_project/view/home.dart';
+import 'package:last_project/controller/dbhelper.dart';
+import 'package:last_project/view/register.dart';
+
+class Login extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login(BuildContext context) async {
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    if (username.isNotEmpty && password.isNotEmpty) {
+      final user = await dbhelper.instance.getUser(username);
+      if (user.isNotEmpty && user['password'] == password) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => home()),
+        );
+      } else {
+        _showDialog(context, 'Invalid username or password!');
+      }
+    } else {
+      _showDialog(context, 'Please fill in all fields!');
+    }
+  }
+
+  void _showDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Message'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Masuk'),
+        backgroundColor: Colors.white10,
+        centerTitle: true,
+        shape: Border.all(
+          color: Colors.black54,
+          width: 3.0,
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/virus.png',
+                height: 120,
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  prefixIcon: Icon(Icons.people),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(color: Colors.blueGrey, width: 2.2),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 10.0),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide:
+                          BorderSide(color: Colors.blueGrey, width: 2.2),
+                    )),
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text('Masuk'),
+                onPressed: () => _login(context),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    textStyle:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+              ),
+              SizedBox(height: 10.0),
+              Text('Masih belum punya akun'),
+              TextButton(
+                child: Text('daftar sekarang'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => register()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
