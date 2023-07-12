c0a85c1bd393782f759427c58791cb596b7a53aa
Alvinaja1 <alvinussalendra029@gmail.com>
2023-07-12 12:13:26 +0700

    doneeee

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,80 +1,42 @@
+import 'package:last_project/view/penyakit1.dart';
 import 'package:flutter/material.dart';
-import 'package:last_project/view/setting.dart';
 
-class home extends StatefulWidget {
-  const home({super.key});
-
-  @override
-  State<home> createState() => _homeState();
-}
-
-class _homeState extends State<home> {
+class homee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Simple Home'),
         centerTitle: true,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'Calls',
+        backgroundColor: Colors.white10,
+        title: Text('wabah penyakit yang berbahaya didunia'),
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'Camera',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chats',
+          IconButton(
+            icon: new Icon(Icons.menu, color: Colors.white),
+            onPressed: () {},
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Welcome to Simple Home!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Simple Home',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                // Navigasi ke halaman utama
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                // Navigasi ke halaman pengaturan
-                Navigator.pop(context);
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SettingsPage()));
-              },
-            ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("covid 19"),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => penyakit1()));
+            },
+          ),
+          ListTile(
+            title: Text("penyakit obstruktif kronis(pok)"),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => penyakit1()));
+            },
+          ),
+        ],
       ),
     );
   }
