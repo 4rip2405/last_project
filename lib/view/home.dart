829b4d49489af9bd8851fe69d41cb029c088c9bf
Palcomtech <ahmadparsaoran8@gmail.com>
2023-07-12 13:16:51 +0700

    home edit

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,80 +1,149 @@
 import 'package:flutter/material.dart';
-import 'package:last_project/view/setting.dart';
 
-class home extends StatefulWidget {
-  const home({super.key});
-
-  @override
-  State<home> createState() => _homeState();
+void main() {
+  runApp(MyApp());
 }
 
-class _homeState extends State<home> {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CRUD Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: home(),
+    );
+  }
+}
+
+class home extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<home> {
+  List<String> items = [];
+
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  void addItem(String item) {
+    setState(() {
+      items.add(item);
+    });
+  }
+
+  void updateItem(int index, String item) {
+    setState(() {
+      items[index] = item;
+    });
+  }
+
+  void deleteItem(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+  }
+
+  void showEditDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController editController =
+            TextEditingController(text: items[index]);
+
+        return AlertDialog(
+          title: Text('Edit Item'),
+          content: TextField(controller: editController),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Save'),
+              onPressed: () {
+                updateItem(index, editController.text);
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Simple Home'),
-        centerTitle: true,
+        title: Text('CRUD Example'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'Calls',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'Camera',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chats',
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to Simple Home!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Simple Home',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    showEditDialog(index);
+                  },
                 ),
-              ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    deleteItem(index);
+                  },
+                ),
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                // Navigasi ke halaman utama
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                // Navigasi ke halaman pengaturan
-                Navigator.pop(context);
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SettingsPage()));
-              },
-            ),
-          ],
-        ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Add Item'),
+                content: TextField(
+                  controller: _textEditingController,
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Save'),
+                    onPressed: () {
+                      addItem(_textEditingController.text);
+                      _textEditingController.clear();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      _textEditingController.clear();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
